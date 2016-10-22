6ee79e8bfc4f6dd85cf843c02619c2d3ffeb1a5e
markaspot <holger@markaspot.org>
2016-10-22 09:14:16 +0200

    Resolve merge conflict

--- geolocation_nominatim.widget.js
+++ geolocation_nominatim.widget.js
@@ -132,15 +132,10 @@
         if ('postcode' in details) {
             $('input.postal-code', $address).val(details.postcode);
         }
-<<<<<<< HEAD
-        if ('city' in details || 'town' in details) {
-            var locality = details.city ? details.city : details.town;
-            $('input.locality', $address).val(locality);
-=======
+
         if ('city' in details || 'town' in details || 'village' in details || 'hamlet' in details) {
             var localityType = details.city || details.town || details.village || details.hamlet;
             $('input.locality', $address).val(localityType);
->>>>>>> use-more-details
         }
         if ('road' in details || 'building' in details || 'footway' in details || 'pedestrian' in details) {
             var streetType = details.road || details.footway || details.pedestrian;
